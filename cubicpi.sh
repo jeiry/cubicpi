cec0891c460b84160cf1da6c145afe37abcf7209
jeiry <gjeiry@gmail.com>
2021-02-10 20:46:44 +0800

    Update cubicpi.sh

--- cubicpi.sh
+++ cubicpi.sh
@@ -4,15 +4,14 @@ tar zxvf bcm2835-1.68.tar.gz
 cd bcm2835-1.68/
 sudo ./configure && sudo make && sudo make check && sudo make install
 sudo apt-get install wiringpi
-#对于树莓派2019年5月之后的系统（早于之前的可不用执行），可能需要进行升级：
 wget https://project-downloads.drogon.net/wiringpi-latest.deb
 sudo dpkg -i wiringpi-latest.deb
 gpio -v
 sudo apt-get install ttf-wqy-zenhei
 sudo apt-get install python3-pip
-sudo pip3 install RPi.GPIO
-sudo pip3 install spidev
-sudo pip3 install numpy
+sudo pip3 install RPi.GPIO --timeout 1000
+sudo pip3 install spidev --timeout 1000
+sudo pip3 install numpy --timeout 1000
 sudo apt-get install p7zip-full
 wget http://www.waveshare.net/w/upload/b/bd/1.3inch_LCD_HAT_code.7z
 7z x 1.3inch_LCD_HAT_code.7z -r -o./1.3inch_LCD_HAT_code
@@ -21,4 +20,3 @@ cd 1.3inch_LCD_HAT_code
 cd c
 make clean
 make
-sudo ./main
